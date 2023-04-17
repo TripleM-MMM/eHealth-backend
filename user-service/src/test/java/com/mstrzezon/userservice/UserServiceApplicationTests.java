1dd6ac7eae4067bd753dbd40ec737ed7cddeaac0
Michał Strzeżoń <mstrzezon@gmail.com>
2023-04-17 22:25:02 +0200

    user-service # Changes: - remove gitignore, - refactor project

--- user-service/src/test/java/com/mstrzezon/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/mstrzezon/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,9 @@
 package com.mstrzezon.userservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
