c722b601feb159c4c2f2b83f701a87369a9d060f
Michał Strzeżoń <mstrzezon@gmail.com>
2023-04-17 22:23:59 +0200

    sensorsdata-service # Changes: - remove gitignore, - refactor project

--- sensorsdata-service/src/test/java/com/mstrzezon/sensorsdataservice/SensorsdataServiceApplicationTests.java
+++ sensorsdata-service/src/test/java/com/mstrzezon/sensorsdataservice/SensorsdataServiceApplicationTests.java
@@ -1,13 +1,10 @@
 package com.mstrzezon.sensorsdataservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SensorsdataServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
