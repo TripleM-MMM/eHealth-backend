20039b2b8048514663ec9599fe2d9a0846f7b78d
Michał Strzeżoń <mstrzezon@gmail.com>
2023-06-09 15:16:01 +0200

    eHealth-backend # Changes: - fix bugs

--- measurements-service/src/main/java/com/mstrzezon/measurements/controller/MeasurementController.java
+++ measurements-service/src/main/java/com/mstrzezon/measurements/controller/MeasurementController.java
@@ -1,12 +1,17 @@
 package com.mstrzezon.measurements.controller;
 
+import com.mstrzezon.measurements.dto.MeasurementDTO;
 import com.mstrzezon.measurements.dto.SynchronizeRequest;
 import com.mstrzezon.measurements.service.GeneralMeasurementsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/measurements")
 @RequiredArgsConstructor
 public class MeasurementController {
 
@@ -16,6 +21,12 @@ public class MeasurementController {
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void synchronizeData(@RequestBody SynchronizeRequest synchronizeRequest) {
         generalMeasurementsService.synchronizeData(synchronizeRequest);
+    }
+
+    @GetMapping("/device/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<MeasurementDTO> getMeasurements(@PathVariable("id") Long id) {
+        return Collections.singletonList(MeasurementDTO.builder().build());
     }
 
 }
